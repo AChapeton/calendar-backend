9b65b678758ecd04a02dc2acd5d9382105bd8cd0
andreschapeton <andres.chapeton@focusservices.com>
2024-04-17 16:22:20 -0600

    added controllers to update and delete events

--- controllers/events.js
+++ controllers/events.js
@@ -30,17 +30,78 @@ const createEvent = async (req, res) => {
 }
 
 const updateEvent = async (req, res) => {
-  return res.status(200).json({
-    ok: true,
-    msg: 'updateEvent'
-  })
+  const eventId = req.params.id
+  const uid = req.uid
+
+  try{
+    const event = await Event.findById( eventId );
+
+    if( !event ){
+      return res.status(404).json({
+        ok: false,
+        msg: 'The event does not exist.'
+      })
+    }
+
+    if( event.user.toString() !== uid){
+      return res.status(401).json({
+        ok: false,
+        msg: 'You do not have permission to update this event.'
+      })
+    }
+
+    const newEvent = {
+      ...req.body,
+      user: uid
+    }
+
+    const updatedEvent = await Event.findByIdAndUpdate( eventId, newEvent, { new: true } )
+
+    return res.status(200).json({
+      ok: true,
+      event: newEvent
+    })
+  }catch(error){
+    res.status(500).json({
+      ok: false,
+      msg: 'There was an error. Talk to an admin about it.'
+    })
+ }
 }
 
 const deleteEvent = async (req, res) => {
-  return res.status(200).json({
-    ok: true,
-    msg: 'deleteEvent'
-  })
+  const eventId = req.params.id
+  const uid = req.uid
+
+  try{
+    const event = await Event.findById( eventId );
+
+    if( !event ){
+      return res.status(404).json({
+        ok: false,
+        msg: 'The event does not exist.'
+      })
+    }
+
+    if( event.user.toString() !== uid){
+      return res.status(401).json({
+        ok: false,
+        msg: 'You do not have permission to delete this event.'
+      })
+    }
+
+    await Event.findByIdAndDelete( eventId )
+
+    return res.status(200).json({
+      ok: true,
+      msg: 'Event deleted successfully'
+    })
+  }catch(error){
+    res.status(500).json({
+      ok: false,
+      msg: 'There was an error. Talk to an admin about it.'
+    })
+ }
 }
 
 module.exports = {
