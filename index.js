a9b35537ba42221b3d5ffd991e42f860740f5f3d
andreschapeton <andres.chapeton@focusservices.com>
2024-04-29 16:08:30 -0600

    deploy changes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const serverless = require('serverless-http')
-const { dbConnection } = require('../database/config')
+const { dbConnection } = require('./database/config')
 require('dotenv').config()
 // Create server express
 const app = express()
@@ -11,24 +10,17 @@ dbConnection()
 
 app.use(cors())
 
-const router = express.Router()
-
 //Public directory
 app.use(express.static('public'))
 
 app.use( express.json() )
 //Routes
 // TODO: Auth routes // create, login, renew token
-app.use('/api/auth', require('../routes/auth'));
+app.use('/api/auth', require('./routes/auth'));
 // TODO: CRUD: Events
-app.use('/api/events', require('../routes/events'));
+app.use('/api/events', require('./routes/events'));
 
 //Listen petitions
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`)
 })
-
-app.use('./.netlify/functions/server', router)
-const handler = serverless(app);
-module.exports = { handler };
-
